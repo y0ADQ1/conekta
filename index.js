cc8e24879d19b770a2fb37358ff6e2936b49f71c
Emilio Olvera <jesusolvera355@gmail.com>
2025-02-25 23:05:46 -0600

    Pura pinche fanny lu hdtpm

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 const { Sequelize } = require('sequelize');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('swagger-jsdoc');
+const swaggerspect ={
+    dfinition:{
+        openapi: "3.0.0",
+        info: {
+            title: "API REST",
+            version: "1.0.0",
+            description: "API REST"
+    },
+    servers: [
+        {
+            url: "http://localhost:8084"
+        }
+    ]
+    },
+    apis: [`${path.join(__dirname, "./routes/*.js")}`]
+}
 
 const router = require('./routes/authRoutes');
 
@@ -19,6 +39,7 @@ app.use(bodyParser.json());
 
 // Registrar rutas
 app.use('/api/auth', router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument(swaggerspect)));
 
 const PORT = process.env.PORT || 8084;
 app.listen(PORT, () => {
