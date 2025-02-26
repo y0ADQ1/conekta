e81fda61934f7f19031eb6645be56bb36cc48c9f
y0ADQ1 <juanderasduron@gmail.com>
2025-02-26 10:51:45 -0600

    papu papu ya quedo conekta papu

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const routes = require('./routes/authRoutes');
+const paymentRoutes = require('./routes/ordernController');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('swagger-jsdoc');
+const swaggerspect ={
+    definition:{
+        openapi: "3.0.0",
+        info: {
+            title: "API REST",
+            version: "1.0.0",
+            description: "API REST"
+        },
+        servers: [
+            {
+                url: "http://localhost:8084"
+            }
+        ]
+    },
+    apis: [`${path.join(__dirname, "./routes/*.js")}`]
+}
 
 const app = express();
 const PORT = process.env.PORT || 8084;
@@ -15,6 +35,8 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', routes);
+app.use('/api/pago', paymentRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument(swaggerspect)));
 
 app.get('/products', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'products.html'));
