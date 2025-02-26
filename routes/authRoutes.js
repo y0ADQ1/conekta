560e56da64b64e20debea05ebdc10643156ac388
Emilio Olvera <jesusolvera355@gmail.com>
2025-02-26 10:54:36 -0600

    ARRIBA LAS NALGOTICAS

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,21 @@ const { getAllProducts } = require('../controllers/productController');
 const { addToCart } = require('../controllers/productController');
 
 // Rutas de API
+/**
+ * @swagger
+ * /api/auth/products:
+ *   get:
+ *     summary: Obtiene todos los productos
+ *     description: Retorna una lista de productos.
+ *     responses:
+ *       200:
+ *         description: Respuesta exitosa
+ *       400:
+ *         description: Solicitud incorrecta
+ */
 router.get('/products', getAllProducts);
+
+
 router.post('/products', addToCart);
 
 module.exports = router;
